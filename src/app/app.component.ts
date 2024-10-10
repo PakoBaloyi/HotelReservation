4e9c90d62d247d9499cfbeddcdbde2d977c506d6
Pako Baloyi <Pbaloyi@hollywoodbets.net>
2024-10-10 15:41:32 +0200

    components creation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
+import { ReservationModule } from './reservation/reservation.module';
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterModule, RouterOutlet } from '@angular/router';
+import { HomeModule } from './home/home.module';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,CommonModule,HomeModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
+import { Component } from '@angular/core';
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { ReservationFormComponent } from './reservation-form/reservation-form.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path:"", component:HomeComponent},
+  {path:"list", component:ReservationFormComponent},
+  {path:"new", component:ReservationFormComponent}
+];
